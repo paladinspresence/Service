952522c634e375f1a86874545f09671a26a85bfd
Matthew Hatcher <matthewh@halfpetal.com>
2019-03-05 15:58:14 -0500

    Adding some supporting code from SO.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.Configuration.Install;
 
 namespace Paladins_Presence
 {
@@ -12,14 +14,64 @@ namespace Paladins_Presence
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main()
+        static void Main(string[] args)
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
+            if (args.Length == 0)
             {
-                new Presence()
-            };
-            ServiceBase.Run(ServicesToRun);
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
+                    new Presence()
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
+            else if (args.Length == 1)
+            {
+                switch (args[0])
+                {
+                    case "--install":
+                        break;
+                    case "--uninstall":
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+
+        private static bool IsInstalled()
+        {
+            using (ServiceController controller =
+                new ServiceController("PaladinsPresence"))
+            {
+                try
+                {
+                    ServiceControllerStatus status = controller.Status;
+                }
+                catch
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        private static bool IsRunning()
+        {
+            using (ServiceController controller =
+                new ServiceController("PaladinsPresence"))
+            {
+                if (!IsInstalled()) return false;
+                return (controller.Status == ServiceControllerStatus.Running);
+            }
+        }
+
+        private static AssemblyInstaller GetInstaller()
+        {
+            AssemblyInstaller installer = new AssemblyInstaller(
+                typeof(Presence).Assembly, null);
+            installer.UseNewContext = true;
+            return installer;
         }
     }
 }
